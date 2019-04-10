df4d367320a5ba90d98066ad84973eddcad41619
Joshua Swick <joshuaswickirl@gmail.com>
2019-04-10 17:43:34 -0600

    Refactor and finish docstrings

--- tree.py
+++ tree.py
@@ -1,48 +1,68 @@
 class Node():
     def __init__(self, input_data=None):
         """
-        Constructor method for class node.
+        Creates new node with input data and an empty list
+        of children nodes.
+
+        Runs in constant time, O(n).
         """
         self.data = input_data
         self.children = []
     
+
     def get_data(self):
         """
-        Gets the data from list, Requires nothing and returns item. 
+        Returns the node's data.
+
+        Runs in constant time, O(n).
         """
         return self.data
 
+
     def set_data(self, new_data):
         """
-        Sets data, requires new_data and returns nothing.
+        Sets the node's data, overwritting existing data.
+
+        Runs in constant time, O(n).
         """
         self.data = new_data
 
+
     def get_children(self):
         """
-        Gets refrence to children nodes. 
+        Returns list of children nodes. 
+
+        Runs in constant time, O(n).
         """
         return self.children
     
+
     def add_child(self, input_data):
         """
-        adds a child node
+        Creates a new node and lists it as a child node.
+
+        Runs in constant time, O(n).
         """
         newNode = Node(input_data)
         self.children.append(newNode)
 
 
-class Tree(Node):
+class Tree():
     def __init__(self, input_data=None):
         """
         Adds tree head and inital Node.
+
+        Runs in constant time, O(1).
         """
         self.head = Node(input_data)
+
 
     def search_for(self, item, node=None):
         """
         Recursively searches for the given item starting at 
         the tree head unless a specific node is given.
+
+        Runs in linear time, O(n).
         """
         item_found = False
         if node == None:
@@ -57,20 +77,24 @@ class Tree(Node):
                     item_found = self.search_for(item,child_node)
         return item_found
 
+
     def is_empty(self):
         """
-        Checks if list is empty, Requires nothing and returns boolean value.
+        Checks if tree is empty.
         
-        Runs in constant time.
+        Runs in constant time, O(1).
         """
-        if self.head.data == None and self.head.children == []:
+        if self.head.get_data() == None and self.head.children == []:
             return True
         else:
             return False
 
+
     def print_tree(self,node=None):
         """
-        Prints tree structure
+        Prints tree structure.
+
+        Runs in linear time, O(n).
         """
         if node == None:
             node = self.head
